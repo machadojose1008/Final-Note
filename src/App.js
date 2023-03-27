e45b83259e23ba325c6d82f1b9b6a6a793daf678
Jose Machado <jgbm81@hotmail.com>
2023-03-27 16:00:41 -0400

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SidebarComponent from './sidebar/sidebar'
+import EditorComponent from './editor/editor'
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebase = require("firebase/app");
+// Required for side-effects
+require("firebase/firestore");
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+
+  render() {
+    return(
+      <div className='app-container'>
+        <SidebarComponent 
+            selectedNoteIndex={this.state.selectedNoteIndex}
+            notes={this.state.notes}
+          ></SidebarComponent>
+        <EditorComponent></EditorComponent>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    const db = getFirestore();
+    const notesRef = collection(db, 'notes');
+  
+    onSnapshot(notesRef, (serverUpdate) => {
+      const notes = serverUpdate.docs.map((_doc) => {
+        const data = _doc.data();
+        data['id'] = _doc.id;
+        return data;
+      });
+      console.log(notes);
+      this.setState({ notes: notes });
+    });
+  };
+
 }
 
 export default App;
