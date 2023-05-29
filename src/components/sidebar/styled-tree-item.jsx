e51c9541ec476063f6c792ef2f58647931558ee5
Jose Machado <jgbm81@hotmail.com>
2023-05-29 15:43:42 -0400

    styled tree item overflow

--- src/components/sidebar/styled-tree-item.jsx
+++ src/components/sidebar/styled-tree-item.jsx
@@ -81,7 +81,7 @@ export function StyledTreeItem(props) {
       label={
         <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
           <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
-          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1, maxWidth:'100px' }}>
+          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1, maxWidth:'100px', overflow:'hidden' }}>
             {labelText}
           </Typography>
           <Typography variant="caption" color="inherit">
